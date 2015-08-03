b0c8e5e59324290451e368b92bd8f9aa66b43228
Paul Denya <pdenya@gmail.com>
2015-08-02 19:05:21 -0700

    style updates

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -3,6 +3,12 @@ class VideosController < ApplicationController
 	def index
 		page = params[:page] ? (params[:page].to_i - 1) : 0
 		@references = Reference.includes(:video).select('video_id, count(*) as count').group('video_id').order('count(*) DESC').limit(10).offset(10*page)
+
+		@vid_by_sub = {}
+		Comment.select('comments.subreddit, "references".video_id AS reference_video_id').joins('INNER JOIN "references" ON comments.id = "references".comment_id').where('video_id IN (?)', @references.map {|r| r.video_id }).each do |comment|
+			@vid_by_sub[comment.reference_video_id] ||= []
+			@vid_by_sub[comment.reference_video_id] << comment.subreddit unless @vid_by_sub[comment.reference_video_id].include?(comment.subreddit)
+		end
 	end
 
 	def show
