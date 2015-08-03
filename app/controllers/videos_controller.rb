e5d5cb2ce2ba099be7fb528d693746d24c8c8c4d
Paul Denya <pdenya@gmail.com>
2015-08-02 17:44:02 -0700

    fixed unicorn stuff and added some styling and pagination

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,7 +1,8 @@
 class VideosController < ApplicationController
 
 	def index
-		@references = Reference.includes(:video).select('video_id, count(*) as count').group('video_id').order('count(*) DESC').limit(10).offset(10)
+		page = params[:page] ? (params[:page].to_i - 1) : 0
+		@references = Reference.includes(:video).select('video_id, count(*) as count').group('video_id').order('count(*) DESC').limit(10).offset(10*page)
 	end
 
 	def show
