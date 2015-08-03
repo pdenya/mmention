d3de752b26ca435800803508c7f09e5abff8b430
Paul Denya <pdenya@gmail.com>
2015-08-02 23:10:27 -0700

    styling, google analytics, subreddit filtering, pagination ui

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,8 +1,12 @@
 class VideosController < ApplicationController
 
 	def index
-		page = params[:page] ? (params[:page].to_i - 1) : 0
-		@references = Reference.includes(:video).select('video_id, count(*) as count').group('video_id').order('count(*) DESC').limit(10).offset(10*page)
+		@page = params[:page] ? params[:page].to_i : 1
+		@references = Reference.includes(:video).select('video_id, count(*) as count').group('video_id').order('count(*) DESC').limit(10).offset(10*(@page - 1))
+
+		if params[:subreddit] && params[:subreddit] =~ /^[A-Za-z0-9_]+$/
+			@references = @references.joins("INNER JOIN comments ON (\"references\".comment_id = comments.id AND comments.subreddit = '#{params[:subreddit]}')")
+		end
 
 		@vid_by_sub = {}
 		Comment.select('comments.subreddit, "references".video_id AS reference_video_id').joins('INNER JOIN "references" ON comments.id = "references".comment_id').where('video_id IN (?)', @references.map {|r| r.video_id }).each do |comment|
